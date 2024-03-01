f295b613c2a3c2f4b1f7452a5053cb7bbe473042
Juanegm22 <juanesgarciamar@outlook.com>
2024-02-29 22:18:43 -0500

    Introduce django messages

--- projects/p02_bookmarks/account/views.py
+++ projects/p02_bookmarks/account/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 from .forms import LoginForm, UserRegistrationForm, ProfileEditForm, UserEditForm
 from .models import Profile
@@ -71,6 +72,9 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, "Profile updated succesfully")
+        else:
+            messages.error(request, "Error updating your profile")
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(
