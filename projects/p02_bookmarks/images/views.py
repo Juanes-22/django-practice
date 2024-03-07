206aea160202a497092b7ddbf222d8085e0fedec
Juanegm22 <juanesgarciamar@outlook.com>
2024-03-06 22:29:18 -0500

    Add images list

--- projects/p02_bookmarks/images/views.py
+++ projects/p02_bookmarks/images/views.py
@@ -8,6 +8,8 @@ from .forms import ImageCreateForm
 
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
+from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 @login_required
@@ -39,7 +41,6 @@ def image_detail(request, id, slug):
     )
 
 
-
 @login_required
 @require_POST
 def image_like(request):
@@ -58,3 +59,35 @@ def image_like(request):
             pass
 
         return JsonResponse({"status": "error"})
+
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 3)
+
+    page = request.GET.get("page")
+    images_only = request.GET.get("images_only")
+
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        images = paginator.page(1)
+    except EmptyPage:
+        if images_only:
+            return HttpResponse("")
+
+        images = paginator.page(paginator.num_pages)
+
+    if images_only:
+        return render(
+            request,
+            "images/image/list_images.html",
+            {"section": "images", "images": images},
+        )
+
+    return render(
+        request,
+        "images/image/list.html",
+        {"section": "images", "images": images},
+    )
