1ba04488ed2c13eafcc536f4d9cd451f6bdd4dbb
Juanegm22 <juanesgarciamar@outlook.com>
2024-03-08 17:44:58 -0500

    Implement users list and detail

--- projects/p02_bookmarks/account/views.py
+++ projects/p02_bookmarks/account/views.py
@@ -5,8 +5,12 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+
 from .forms import LoginForm, UserRegistrationForm, ProfileEditForm, UserEditForm
 from .models import Profile
+
 
 def user_login(request):
     if request.method == "POST":
@@ -36,37 +40,30 @@ def dashboard(request):
     return render(request, "account/dashboard.html", {"section": "dashboard"})
 
 
-
 def register(request):
     if request.method == "POST":
         user_form = UserRegistrationForm(request.POST)
         if user_form.is_valid():
             new_user = user_form.save(commit=False)
             password = user_form.cleaned_data["password"]
-            new_user.set_password(
-                password
-            )
+            new_user.set_password(password)
             new_user.save()
 
             Profile.objects.create(user=new_user)
-            
+
             return render(request, "account/register_done.html", {"new_user": new_user})
     else:
         user_form = UserRegistrationForm()
 
     return render(request, "account/register.html", {"user_form": user_form})
 
+
 @login_required
 def edit(request):
     if request.method == "POST":
-        user_form = UserEditForm(
-            instance=request.user,
-            data=request.POST
-        )
+        user_form = UserEditForm(instance=request.user, data=request.POST)
         profile_form = ProfileEditForm(
-            instance=request.user.profile,
-            data=request.POST,
-            files=request.FILES
+            instance=request.user.profile, data=request.POST, files=request.FILES
         )
 
         if user_form.is_valid() and profile_form.is_valid():
@@ -77,12 +74,28 @@ def edit(request):
             messages.error(request, "Error updating your profile")
     else:
         user_form = UserEditForm(instance=request.user)
-        profile_form = ProfileEditForm(
-            instance=request.user.profile
-        )
+        profile_form = ProfileEditForm(instance=request.user.profile)
 
     return render(
         request,
         "account/edit.html",
-        {"user_form": user_form, "profile_form": profile_form}
+        {"user_form": user_form, "profile_form": profile_form},
+    )
+
+
+@login_required
+def user_list(request):
+    users = User.objects.filter(is_active=True)
+    return render(
+        request, "account/user/list.html", {"section": "people", "users": users}
+    )
+
+
+@login_required
+def user_detail(request, username):
+    user = get_object_or_404(User, username=username, is_active=True)
+    return render(
+        request,
+        "account/user/detail.html",
+        {"section": "people", "user": user},
     )
--- projects/p02_bookmarks/p02_bookmarks/settings.py
+++ projects/p02_bookmarks/p02_bookmarks/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+from django.urls import reverse_lazy
 
 load_dotenv()
 
@@ -173,3 +174,7 @@ SOCIAL_AUTH_PIPELINE = [
     "social_core.pipeline.social_auth.load_extra_data",
     "social_core.pipeline.user.user_details",
 ]
+
+ABSOLUTE_URL_OVERRIDES = {
+    "auth.user": lambda u: reverse_lazy("user_detail", args=[u.username])
+}
