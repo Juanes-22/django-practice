b97c0b7bd41b98746da55dc3df6ee574487ae134
Juanegm22 <juanesgarciamar@outlook.com>
2024-03-19 23:16:49 -0500

    Implement image views and ranking using Redis database

--- projects/p02_bookmarks/images/views.py
+++ projects/p02_bookmarks/images/views.py
@@ -13,6 +13,17 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from actions.utils import create_action
 
+import redis
+from django.conf import settings
+
+
+r = redis.Redis(
+    host=settings.REDIS_HOST,
+    port=settings.REDIS_PORT,
+    db=settings.REDIS_DB,
+)
+
+
 @login_required
 def image_create(request):
     if request.method == "POST":
@@ -25,11 +36,7 @@ def image_create(request):
             new_image.user = request.user
             new_image.save()
 
-            create_action(
-                request.user,
-                "bookmarked image",
-                new_image
-            )
+            create_action(request.user, "bookmarked image", new_image)
 
             messages.success(request, "Image added successfully")
             return redirect(new_image.get_absolute_url())
@@ -43,8 +50,27 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+    total_views = r.incr(f"image:{image.id}:views")
+    r.zincrby("image_ranking", 1, image.id)
     return render(
-        request, "images/image/detail.html", {"section": "images", "image": image}
+        request,
+        "images/image/detail.html",
+        {"section": "images", "image": image, "total_views": total_views},
+    )
+
+
+@login_required
+def image_ranking(request):
+    image_ranking = r.zrange("image_ranking", 0, -1, desc=True)[:10]
+    image_ranking_ids = [int(id) for id in image_ranking]
+
+    # get most viewed images
+    most_viewed = list(Image.objects.filter(id__in=image_ranking_ids))
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+    return render(
+        request,
+        "images/image/ranking.html",
+        {"section": "images", "most_viewed": most_viewed},
     )
 
 
